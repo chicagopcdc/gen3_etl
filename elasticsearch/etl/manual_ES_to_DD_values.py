86a8bc7a2d3f067405158fd4f29b46d6a22cbc59
paulmurdoch19 <paulmurdoch19@gmail.com>
2025-01-14 10:57:35 -0800

    create mapping from ES values to DD values

---
+++ elasticsearch/etl/manual_ES_to_DD_values.py
@@ -0,0 +1,21 @@
+config = {
+    'biospecimen_status':
+        {
+            "pointer": None,
+            "enum": [
+                'Absent',
+                'Present'
+            ]
+        
+        },
+    'lkss_obfuscated': 
+        {
+            "pointer": None,
+            "enum": [
+                'Known',
+                'Unknown'
+            ]
+        
+        }
+
+}
